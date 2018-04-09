d8120473fe3cacc43374484494c5c22a5f564ea0
sklmix <33098906+sklmix@users.noreply.github.com>
2018-04-09 23:51:40 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
-
-<?php include'home.php';?>
+<!DOCTYPE HTML>
+<head>
+<title> Error</title>
+<style> 
+     .content{}
+	 .content h2{
+		 font-size: 100px;
+		 text-align: center;
+	 }
+	</style>
+	<script> window.location='http://localhost/blood';</script>
+	<?php include 'include/head.php';?>
+</head>
+<body>
+<div class="content">
+     <h2> 404 <span>Not Found</span></h2>
+	</div>
+</body>
